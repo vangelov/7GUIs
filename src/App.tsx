c3aad81753a43d671c36a19faa13947f007f13bb
Vladimir Angelov <vlady.angelov@gmail.com>
2023-01-02 18:38:26 +0200

    Add README and link to Github

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,13 @@ function App() {
   return (
     <div>
       <h1>7GUIs</h1>
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://github.com/vangelov/7GUIs"
+      >
+        View Github repo
+      </a>
       <hr />
       <Counter />
       <hr />
